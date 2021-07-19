d1d8f3ddaef1578398b7e756d5359288fa05038f
Abhishek Sijariya <87616885+Abhisijariya009@users.noreply.github.com>
2021-07-19 19:29:32 +0530

    Update settings.py

--- Ecomm/Ecomm/settings.py
+++ Ecomm/Ecomm/settings.py
@@ -144,6 +144,7 @@ STATIC_FILES_DIR = [
 MEDIA_URL = '/images/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'static/images')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 django_heroku.settings(locals())
 # Default primary key field type
